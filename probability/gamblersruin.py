684f71c179c67420e15ead919a6e548fb99dc736
Jacob <47821203+ja-ox@users.noreply.github.com>
2019-03-25 14:40:43 +0000

    Update gamblersruin.py

--- probability/gamblersruin.py
+++ probability/gamblersruin.py
@@ -12,13 +12,12 @@ def gamblerWalk(start=10,n=100,m=10,p=0.5,win=1,lose=-1,llim=0,ulim=None):
     Y[:,0] = start
     Y = np.cumsum(Y, axis=1)
     
-    # Flatten score once reaches lower/upper limit
     for i, y in enumerate(Y):
+        # Flatten score once reaches lower/upper limit
         if llim in y:
             y[list(y).index(llim):] = llim
         if ulim and ulim in y:
             y[list(y).index(ulim):] = ulim
-        
         Y[i] = y
         # Plot walk
         plt.plot(x,y)
@@ -27,7 +26,6 @@ def gamblerWalk(start=10,n=100,m=10,p=0.5,win=1,lose=-1,llim=0,ulim=None):
     plt.title("Gambler's Ruin")
     plt.xlabel("Number of steps")
     plt.ylabel("Winnings")
-    
     return Y
 
 if __name__ == "__main__":
